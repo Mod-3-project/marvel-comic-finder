103562aa880dfa6f97664cbe1b5f2b6987775207
Nathaniel W <52637288+nw-github@users.noreply.github.com>
2024-01-23 12:29:40 -0500

    create types for API response

--- src/fetch-functions.ts
+++ src/fetch-functions.ts
@@ -1,12 +1,24 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
+const API_HOST = "https://gateway.marvel.com:443";
 
-            export const fetchInitialComics = async (): Promise<object | null> => {
-    const url = `https://gateway.marvel.com:443/v1/public/comics?apikey=${API_KEY}`;
+export type Comic = {
+    id: number;
+    title: string;
+    issueNumber: number;
+    description: string;
+    resourceURI: string; // host/v1/public/comics/{id}
+};
+
+export type ComicDataWrapper = {
+    data: {
+        results: Comic[];
+    };
+};
+
+export const fetchInitialComics = async () => {
+    const url = `${API_HOST}/v1/public/comics?apikey=${API_KEY}`;
     try {
-        const response = await fetch(url);
-        const comics = await response.json();
-        console.log(comics);
-        return comics;
+        return await fetch(url).then(resp => resp.json()) as Comic;
     } catch (error: unknown) {
         if (error instanceof Error) {
             console.warn(error.message);
