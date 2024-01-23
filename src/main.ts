25e0be00e8b5a59836e248bf222c82beffa46100
Nathaniel W <52637288+nw-github@users.noreply.github.com>
2024-01-23 12:57:02 -0500

    pick a random list of comics

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
 import { fetchComic, fetchInitialComics } from "./fetch-functions";
 
+const randInt = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const pickRandom = <T>(element: T[], count: number) => {
+    const result = Array(count);
+    for (let i = 0; i < count; i++) {
+        result[i] = element.splice(randInt(0, element.length), 1);
+    }
+    return result;
+};
+
 const main = async () => {
-    console.log(await fetchInitialComics());
+    const initialComics = pickRandom((await fetchInitialComics())!.data.results, 6);
+    // TODO: display the comics
+    console.log(initialComics);
+
     console.log(await fetchComic(331));
 };
 
